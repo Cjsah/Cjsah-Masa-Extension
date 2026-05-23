c600a2079ca8231e6b46b5abc86eeb19926a2dc1
Cjsah <cjsah@cjsah.net>
2026-05-23 13:31:19 +0800

    update dependency

--- src/main/java/net/cjsah/mod/masaextension/mixin/compat/minihud/EntitiesDataManagerMixin.java
+++ src/main/java/net/cjsah/mod/masaextension/mixin/compat/minihud/EntitiesDataManagerMixin.java
@@ -2,7 +2,12 @@ package net.cjsah.mod.masaextension.mixin.compat.minihud;
 
 import fi.dy.masa.minihud.data.EntitiesDataManager;
 import net.cjsah.mod.masaextension.handler.CrossServerHandler;
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,4 +21,10 @@ public class EntitiesDataManagerMixin {
         CrossServerHandler.parseMetadata(data);
     }
 
+    @Inject(method = "handleBlockEntityData", at = @At(value = "INVOKE", target = "Ljava/util/concurrent/ConcurrentHashMap;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"))
+    private void receiveBlockEntityData(BlockPos pos, CompoundTag nbt, ResourceLocation type, CallbackInfoReturnable<BlockEntity> cir) {
+        if (!nbt.contains("Items")) return;
+        ListTag items = nbt.getList("Items", Tag.TAG_COMPOUND);
+
+    }
 }
